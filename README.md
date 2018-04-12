51c96e36213448f6421b27c41fdc0558c330a68e
Firoz Khan <firozk@codeaurora.org>
2018-04-12 11:47:00 +0530

    arch: microblaze: Add system call table generator to generate include/uapi/asm/unistd_32.h and kernel/syscall_table.S
    - Create syscalltbl.sh(new) by combine syscallhdr.sh and syscalltbl.sh
    - Modify to Makefile to support syscalltbl.sh
    - Validate the script by replacing arch/x86/entry/syscalls/*

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - Add x86 Reference script
 
 - Add microblaze script
+      - Create syscalltbl.sh(new) by combine syscallhdr.sh and syscalltbl.sh
+      - Modify to Makefile to support syscalltbl.sh
--- syscalltbl.sh
+++ syscalltbl.sh
@@ -3,21 +3,44 @@
 in="$1"
 out="$2"
 my_abis=`echo "($3)" | tr ',' '|'`
+prefix="$4"
+offset="$5"
 
-nxt=0;
+if [ ${out: -2} == ".h" ]; then
+    fileguard=_UAPI_ASM_MICROBLAZE_`basename "$out" | sed \
+    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
+    -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
+    grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	echo "#ifndef ${fileguard}"
+	echo "#define ${fileguard}"
+	echo ""
 
-grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-    echo "ENTRY(sys_call_table)"
+	while read nr abi name entry ; do
+	    if [ -z "$offset" ]; then
+		echo -e "#define __NR_${prefix}${name}\t$nr"
+	    else
+		echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
+            fi
+	done
 
-    while read nr abi name entry ; do
-	if [ "$nxt" -ne "$nr" ]; then
-	    while [ "$nxt" -lt "$nr" ]; do
-		echo -e "\t.long sys_ni_syscall"
-		let nxt=nxt+1
-	    done
-	fi
-	echo -e "\t.long sys_${name}"
-	nxt="$nr"
-	let nxt=nxt+1
-    done
-) > "$out"
+	echo ""
+	echo "#endif /* ${fileguard} */"
+    ) > "$out"
+elif [ ${out: -2} == ".S" ]; then
+    nxt=0
+    grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	echo "ENTRY(sys_call_table)"
+
+	while read nr abi name entry ; do
+	    if [ "$nxt" -ne "$nr" ]; then
+		while [ "$nxt" -lt "$nr" ]; do
+		    echo -e "\t.long sys_ni_syscall"
+		    let nxt=nxt+1
+		done
+	    fi
+	    echo -e "\t.long sys_${name}"
+	    nxt="$nr"
+	    let nxt=nxt+1
+	done
+    ) > "$out"
+fi
