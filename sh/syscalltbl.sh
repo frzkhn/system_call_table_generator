ff1f0dd51af3630cdbe21753762058fae624f144
Firoz Khan <firozk@codeaurora.org>
2018-04-13 16:13:22 +0530

    arch: sh: Add system call table generator to generate include/uapi/asm/unistd_32/64.h and kernel/syscall_32/64.S
    - Modify syscall.tbl to e_syscall.tbl by hand to have *no* change
      w/ old model.
    NOTE: Marcin's script will generate syscall_32/64.tbl
    - Create syscalltbl.sh(new) by combine syscallhdr.sh and syscalltbl.sh
    - Modify Makefile to support syscalltbl.sh
    - Validate the script by replacing arch/x86/entry/syscalls/*

---
+++ sh/syscalltbl.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+in="$1"
+out="$2"
+my_abis=`echo "($3)" | tr ',' '|'`
+prefix="$4"
+offset="$5"
+
+if [ ${out: -2} == ".h" ]; then
+    nxt=0
+    fileguard=_UAPI_ASM_SH_`basename "$out" | sed \
+    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
+    -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
+    grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	echo "#ifndef ${fileguard}"
+	echo "#define ${fileguard}"
+	echo ""
+
+	while read nr abi name entry ; do
+	    if [ -z "$offset" ]; then
+		echo -e "#define __NR_${prefix}${name}\t$nr"
+	    else
+		echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
+            fi
+	    nxt="$nr"
+	    let nxt=nxt+1
+	done
+	
+	echo ""
+	echo -e "#define NR_syscalls\t$nxt"
+
+	echo ""
+	echo "#endif /* ${fileguard} */"
+    ) > "$out"
+elif [ ${out: -2} == ".S" ]; then
+    nxt=0
+    grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	read nr abi name entry
+	if [ "$abi" == "32" ]; then
+	    echo "#include <linux/sys.h>"
+	    echo "#include <linux/linkage.h>"
+	    echo ""
+            echo -e "\t.data"
+	    echo "ENTRY(sys_call_table)"
+	else 
+	    echo "#include <linux/sys.h>"
+	    echo ""
+	    echo -e "\t.section .data, \"aw"\"
+	    echo -e "\t.balign 32"
+	    echo -e "\t.globl sys_call_table"
+	    echo "sys_call_table:"
+	fi
+
+	if [ "$nxt" -ne "$nr" ]; then
+            while [ "$nxt" -lt "$nr" ]; do
+                echo -e "\t.long sys_ni_syscall"
+                let nxt=nxt+1
+            done
+	fi
+	echo -e "\t.long sys_${name}"
+	nxt="$nr"
+	let nxt=nxt+1
+	while read nr abi name entry ; do
+	    if [ "$nxt" -ne "$nr" ]; then
+		while [ "$nxt" -lt "$nr" ]; do
+		    echo -e "\t.long sys_ni_syscall"
+		    let nxt=nxt+1
+		done
+	    fi
+	    if [ "${name}" == "mmap" ]; then
+		echo -e "\t.long old_${name}"
+	    elif [ "${name}" == "ptrace" ] && 
+		[ "$abi" == "64" ]; then
+                echo -e "\t.long sh64_${name}"
+            else
+		 echo -e "\t.long sys_${name}"
+            fi
+	    nxt="$nr"
+	    let nxt=nxt+1
+	done
+    ) > "$out"
+fi
