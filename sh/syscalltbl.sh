b1534996b7330b95575a365452af528443318924
Firoz Khan <firozk@codeaurora.org>
2018-04-19 14:49:01 +0530

    arch: sh: Add system call table generator to generate include/uapi/asm/unistd_32/64.h and kernel/syscall_32/64.S
    - Cleanup the script

--- sh/syscalltbl.sh
+++ sh/syscalltbl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
 
 in="$1"
 out="$2"
@@ -8,7 +9,7 @@ offset="$5"
 
 if [ ${out: -2} == ".h" ]; then
     nxt=0
-    fileguard=_UAPI_ASM_SH_`basename "$out" | sed \
+    fileguard=__ASM_SH_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
@@ -35,14 +36,13 @@ if [ ${out: -2} == ".h" ]; then
 elif [ ${out: -2} == ".S" ]; then
     nxt=0
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	read nr abi name entry
-	if [ "$abi" == "32" ]; then
+	if [ "$3" -eq 32 ]; then
 	    echo "#include <linux/sys.h>"
 	    echo "#include <linux/linkage.h>"
 	    echo ""
             echo -e "\t.data"
 	    echo "ENTRY(sys_call_table)"
-	else 
+	elif [ "$3" -eq 64 ]; then 
 	    echo "#include <linux/sys.h>"
 	    echo ""
 	    echo -e "\t.section .data, \"aw"\"
@@ -51,15 +51,6 @@ elif [ ${out: -2} == ".S" ]; then
 	    echo "sys_call_table:"
 	fi
 
-	if [ "$nxt" -ne "$nr" ]; then
-            while [ "$nxt" -lt "$nr" ]; do
-                echo -e "\t.long sys_ni_syscall"
-                let nxt=nxt+1
-            done
-	fi
-	echo -e "\t.long sys_${name}"
-	nxt="$nr"
-	let nxt=nxt+1
 	while read nr abi name entry ; do
 	    if [ "$nxt" -ne "$nr" ]; then
 		while [ "$nxt" -lt "$nr" ]; do
