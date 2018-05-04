96d22ac57714a8068ca2a758c81afd2262109dcc
Firoz Khan <firozk@codeaurora.org>
2018-05-04 09:10:21 +0530

    arch: ia64: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/syscall_table.S
    - Add comment

--- ia64/syscalltbl.sh
+++ ia64/syscalltbl.sh
@@ -12,6 +12,13 @@ if [ ${out: -2} == ".h" ]; then
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
+	echo "/*"
+	echo " * IA-64 Linux syscall numbers and inline-functions."
+	echo " *"
+	echo " * Copyright (C) 1998-2005 Hewlett-Packard Co"
+	echo " * David Mosberger-Tang <davidm@hpl.hp.com>"
+	echo " */"
 	echo "#ifndef ${fileguard}"
 	echo "#define ${fileguard}"
 	echo ""
@@ -38,6 +45,7 @@ if [ ${out: -2} == ".h" ]; then
 elif [ ${out: -2} == ".S" ]; then
     nxt=1024
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
+	echo "/* SPDX-License-Identifier: GPL-2.0 */"
 	echo -e "\t.rodata"
 	echo -e "\t.align 8"
 	echo -e "\t.globl sys_call_table"
@@ -46,16 +54,24 @@ elif [ ${out: -2} == ".S" ]; then
 	while read nr abi name entry ; do
 	    if [ "$nxt" -ne "$nr" ]; then
 	        while [ "$nxt" -lt "$nr" ]; do
-		    echo -e "\tdata8 sys_ni_syscall"
+		    if [ $(($nxt % 5)) -eq 0 ]; then
+			echo -e "\tdata8 sys_ni_syscall\t/* $nxt */"
+		    else
+			echo -e "\tdata8 sys_ni_syscall"
+		    fi
 		    let nxt=nxt+1
 		done
 	    fi
-	    echo -e "\tdata8 ${entry}"
+	    if [ $(($nxt % 5)) -eq 0 ]; then
+		echo -e "\tdata8 ${entry}\t/* $nxt */"
+	    else
+		echo -e "\tdata8 ${entry}"
+	    fi
 	    nxt="$nr"
 	    let nxt=nxt+1
 	done
 
 	echo ""
-	echo -e "\t.org sys_call_table + 8*NR_syscalls"
+	echo -e "\t.org sys_call_table + 8*NR_syscalls\t/* guard against failures to increase NR_syscalls */"
     ) > "$out"
 fi
