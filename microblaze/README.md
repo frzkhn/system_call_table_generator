cb84ed10d4d3f517a519ec9c4c24796bb953bb3c
Firoz Khan <firozk@codeaurora.org>
2018-05-17 11:38:47 +0530

    arch: microblaze: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/syscall_table.S
    - Cleanup the script

--- microblaze/README.md
+++ microblaze/README.md
@@ -1,11 +1,4 @@
 # system_call_table_generator
 
-- Add microblaze script
-      > - Modify syscall.tbl to e_syscall.tbl by hand.
-      - Join the syscall.tbl and e_syscall.tbl
-      	$ join -a1 syscall.tbl e_syscall.tbl  | grep -v \# | while read a b c d e ; do echo -e "$a\t$b\t$c\tsys_$e" ; done > tmp.tbl
-	$ sed -e 's:\<sys_\>:sys_ni_syscall:' tmp.tbl > syscall.tbl
-NOTE: Marcin's script will generate syscall.tbl
-      - Create syscalltbl.sh(new) by combine syscallhdr.sh and syscalltbl.sh
-      - Modify to Makefile to support syscalltbl.sh
-      - Validate the script by replacing arch/x86/entry/syscalls/*
+This script will generate arch/microblaze/include/uapi/asm/unistd.h
+and arch/microblaze/kernel/syscall_table.S by reading syscall.tbl file
--- microblaze/syscalltbl.sh
+++ microblaze/syscalltbl.sh
@@ -7,7 +7,7 @@ my_abis=`echo "($3)" | tr ',' '|'`
 prefix="$4"
 offset="$5"
 
-if [ ${out: -2} == ".h" ]; then
+if [ "${out: -2}" = ".h" ]; then
     fileguard=_UAPI_ASM_MICROBLAZE_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
@@ -18,7 +18,7 @@ if [ ${out: -2} == ".h" ]; then
 	echo " * Copyright (C) 2006 Atmark Techno, Inc."
 	echo " *"
 	echo " * This file is subject to the terms and conditions of the GNU General Public"
-	echo " * License. See the file "COPYING" in the main directory of this archive"
+	echo " * License. See the file "\"COPYING"\" in the main directory of this archive"
 	echo " * for more details."
 	echo " */"
 	echo ""
@@ -30,40 +30,46 @@ if [ ${out: -2} == ".h" ]; then
 	    if [ -z "$offset" ]; then
 		if [ -z "$comment" ]; then
 		    echo -e "#define __NR_${prefix}${name}\t$nr"
+		elif [ "$comment" = "-" ]; then
+		    echo -e "/* #define __NR_${prefix}${name}\t$nr */"
 		else
 		    echo -e "#define __NR_${prefix}${name}\t$nr\t$comment"
 		fi
 	    else
-		echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
+		if [ -z "$comment" ]; then
+		    echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
+		elif [ "$comment" = "-" ]; then
+		    echo -e "/* #define __NR_${prefix}${name}\t($offset + $nr) */"
+		else
+		    echo -e "#define __NR_${prefix}${name}\t($offset + $nr)\t$comment"
+		fi
             fi
 	done
 
 	echo ""
 	echo "#endif /* ${fileguard} */"
     ) > "$out"
-elif [ ${out: -2} == ".S" ]; then
+elif [ "${out: -2}" = ".S" ]; then
     nxt=0
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
 	echo "/* SPDX-License-Identifier: GPL-2.0 */"
 	echo "ENTRY(sys_call_table)"
 
 	while read nr abi name entry comment ; do
-	    if [ "$nxt" -ne "$nr" ]; then
-		while [ "$nxt" -lt "$nr" ]; do
-		    if [ $(($nxt % 5)) -eq 0 ]; then
-			echo -e "\t.long sys_ni_syscall\t/* $nxt */"
-		    else
-			echo -e "\t.long sys_ni_syscall"
-		    fi
-		    let nxt=nxt+1
-		done
-	    fi
+	    while [ $nxt -lt $nr ]; do
+		if [ $(($nxt % 5)) -eq 0 ]; then
+		    echo -e "\t.long sys_ni_syscall\t/* $nxt */"
+		else
+		    echo -e "\t.long sys_ni_syscall"
+		fi
+		let nxt=nxt+1
+	    done
 	    if [ $(($nr % 5)) -eq 0 ]; then
 		echo -e "\t.long ${entry}\t/* $nr */"
 	    else
 		echo -e "\t.long ${entry}"
 	    fi
-	    nxt="$nr"
+	    nxt=$nr
 	    let nxt=nxt+1
 	done
     ) > "$out"
