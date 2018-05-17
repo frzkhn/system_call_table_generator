0cedee528b6f1bdcc141ee12764a5671dc065521
Firoz Khan <firozk@codeaurora.org>
2018-05-17 15:22:54 +0530

    arch: sparc: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/systbls_32/64.S
    - Fix multi-abi support
    - Cleanup the script

--- sparc/README.md
+++ sparc/README.md
@@ -1,8 +1,4 @@
+# system_call_table_generator
 
-$ cut -c 12- systbls_32.S > e_systbls_32.S /* Revome 1st 12 charactor from every lone in the file */
-$ rm systbls_32.S; mv e_systbls_32.S systbls_32.S
-$ while read a b c d e ; do echo -e ".long $a\n .long $b\n .long $c\n .long $d\n .long $e\n" ; done < systbls_32.S > e_systbls_32.S
-
-$ cut -c 12- systbls_64.S > e_systbls_64.S /* Revome 1st 12 charactor from every lone in the file */
-$ rm systbls_64.S; mv e_systbls_64.S systbls_32.S
-$ while read a b c d e ; do echo -e ".long $a\n.long $b\n.long $c\n.long $d\n.long $e" ; done < systbls_64.S > e_systbls_64/x32.S
+This script will generate arch/sparc/include/uapi/asm/unistd.h
+and arch/sparc/kernel/systbls_32/64.S by reading syscall.tbl file
--- sparc/syscalltbl.sh
+++ sparc/syscalltbl.sh
@@ -3,11 +3,11 @@
 
 in="$1"
 out="$2"
-my_abis=`echo "($3)" | tr ',' '|'`
+abi="$3"
 prefix="$4"
 offset="$5"
 
-if [ ${out: -2} == ".h" ]; then
+if [ "${out: -2}" = ".h" ]; then
     nxt=0
     fileguard=_UAPI_SPARC_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
@@ -15,13 +15,14 @@ if [ ${out: -2} == ".h" ]; then
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
 	echo "#ifndef ${fileguard}"
 	echo "#define ${fileguard}"
-	echo "
-#ifndef __32bit_syscall_numbers__
-#ifndef __arch64__
-#define __32bit_syscall_numbers__
-#endif
-#endif
-"
+	echo ""
+	echo "#ifndef __32bit_syscall_numbers__"
+	echo "#ifndef __arch64__"
+	echo "#define __32bit_syscall_numbers__"
+	echo "#endif"
+	echo "#endif"
+	echo ""
+
 	while read nr name entry_64 entry_32 entry_x32 ; do
 	    if [ -z "$offset" ]; then
 		if [ "$entry_32" != "sys_nis_syscall" -a "$entry_64" == "sys_nis_syscall" ]; then
@@ -38,54 +39,65 @@ if [ ${out: -2} == ".h" ]; then
 	    else
 		echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
             fi
-	    nxt="$nr"
+	    nxt=$nr
 	    let nxt=nxt+1
 	done
 	
-	echo -e "
-#define NR_syscalls\t$nxt
-"
-	echo -e "
-#define KERN_FEATURE_MIXED_MODE_STACK\t0x00000001
-"
+	echo ""
+	echo -e "#define NR_syscalls\t$nxt"
+	echo ""
+	echo -e "#define KERN_FEATURE_MIXED_MODE_STACK\t0x00000001"
+	echo ""
 	echo "#endif /* ${fileguard} */"
     ) > "$out"
-elif [ ${out: -2} == ".S" ]; then
+elif [ "${out: -2}" = ".S" ]; then
     nxt=0
     arr_entry64=()
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	if [ "$abi" == "32" ]; then
+	if [ "$abi" = "32" ]; then
 	    echo -e "\t.data"
             echo -e "\t.align 4"
             echo -e "\t.globl sys_call_table"
             echo "sys_call_table:"
             while read nr name entry_64 entry_32 entry_x32 ; do
-		if [ "$nxt" -ne "$nr" ]; then
-                    while [ "$nxt" -lt "$nr" ]; do
+                while [ $nxt -lt $nr ]; do
+		    if [ $(($nxt % 5)) -eq 0 ]; then
+			echo -e "\t.long sys_nis_syscall\t/* $nxt */"
+		    else
 			echo -e "\t.long sys_nis_syscall"
-			let nxt=nxt+1
-                    done
+		    fi
+		    let nxt=nxt+1
+                done
+		if [ $(($nr % 5)) -eq 0 ]; then
+		    echo -e "\t.long ${entry_32}\t/* $nxt */"
+		else
+		    echo -e "\t.long ${entry_32}"
 		fi
-		echo -e "\t.long ${entry_32}"
-		nxt="$nr"
+		nxt=$nr
 		let nxt=nxt+1
             done
-	elif [ "$abi" == "64" ]; then
+	elif [ "$abi" = "64" ]; then
             echo -e "\t.text"
             echo -e "\t.align  4"
             echo "#ifdef CONFIG_COMPAT"
             echo -e "\t.globl sys_call_table32"
             echo "sys_call_table32:"
             while read nr name entry_64 entry_32 entry_x32 ; do
-                if [ "$nxt" -ne "$nr" ]; then
-                    while [ "$nxt" -lt "$nr" ]; do
-                        echo -e "\t.word sys_nis_syscall"
-                        arr_entry64+=("sys_nis_syscall")
-			let nxt=nxt+1
-                    done
-                fi
-                echo -e "\t.word ${entry_x32}"
-                nxt="$nr"
+                while [ $nxt -lt $nr ]; do
+		    if [ $(($nxt % 5)) -eq 0 ]; then
+			echo -e "\t.word sys_nis_syscall\t/* $nxt */"
+		    else
+			echo -e "\t.word sys_nis_syscall"
+		    fi
+                    arr_entry64+=("sys_nis_syscall")
+		    let nxt=nxt+1
+                done
+		if [ $(($nr % 5)) -eq 0 ]; then
+		    echo -e "\t.word ${entry_x32}\t/* $nxt */"
+		else
+                    echo -e "\t.word ${entry_x32}"
+		fi
+                nxt=$nr
 		arr_entry64+=("${entry_64}")
                 let nxt=nxt+1
             done
@@ -97,8 +109,12 @@ elif [ ${out: -2} == ".S" ]; then
 	    echo "sys_call_table:"
 	    nr_max=$nxt
 	    nxt=0
-	    while [ "$nxt" -lt "$nr_max" ]; do
-		echo -e "\t.word ${arr_entry64[nxt]}"
+	    while [ $nxt -lt $nr_max ]; do
+		if [ $(($nxt % 5)) -eq 0 ]; then
+		    echo -e "\t.word ${arr_entry64[nxt]}\t/* $nxt */"
+		else
+		    echo -e "\t.word ${arr_entry64[nxt]}"
+		fi
 		let nxt=nxt+1
 	    done
 	fi
