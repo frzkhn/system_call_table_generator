d75a4d3831b692bf4e39b322650e9fe03a602b81
Firoz Khan <firozk@codeaurora.org>
2018-05-17 16:21:16 +0530

    arch: parisc: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/syscall_table.S
    - Fix multi-abi support
    - Cleanup the script

--- parisc/README.md
+++ parisc/README.md
@@ -1,9 +1,4 @@
 # system_call_table_generator
 
-- Add pa-risc script
-      > - Modify syscall.tbl to e_syscall.tbl by hand.
-      $ join -a1 syscall.tbl e_syscall.tbl  | grep -v \# | while read a b c d e ; do echo -e "$a\t$b\t$c\tENTRY_SAME($e)" ; done > syscall.tbl
-NOTE: Marcin's script will generate syscall.tbl
-      - Create syscalltbl.sh(new) by combine syscallhdr.sh and syscalltbl.sh
-      - Modify to Makefile to support syscalltbl.sh
-      - Validate the script by replacing arch/x86/entry/syscalls/*
+This script will generate arch/parisc/include/uapi/asm/unistd.h
+and arch/parisc/kernel/syscall_table.S by reading syscall.tbl file
--- parisc/syscalltbl.sh
+++ parisc/syscalltbl.sh
@@ -3,11 +3,11 @@
 
 in="$1"
 out="$2"
-my_abis=`echo "($3)" | tr ',' '|'`
+abi="$3"
 prefix="$4"
 offset="$5"
 
-if [ ${out: -2} = ".h" ]; then
+if [ "${out: -2}" = ".h" ]; then
     fileguard=_UAPI_ASM_PARISC_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`_
@@ -44,18 +44,18 @@ if [ ${out: -2} = ".h" ]; then
 	echo ""
 	echo "#endif /* ${fileguard} */"
     ) > "$out"
-elif [ ${out: -2} = ".S" ]; then
+elif [ "${out: -2}" = ".S" ]; then
     nxt=2
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
 	echo "/* SPDX-License-Identifier: GPL-2.0 */"
 	while read nr name entry_64 entry_32 entry_x32 comment ; do
-	    if [ $3 -eq 64 ]; then
+	    if [ "$3" = "64" ]; then
 		if [ $nr -eq 0 ]; then
 		    echo -e "90:     .dword ${entry_64}\t/* $nr */"
 		elif [ $nr -eq 1 ]; then
 		    echo -e "91:     .dword ${entry_64}"
 		else
-		    while [ "$nxt" -lt "$nr" ]; do
+		    while [ $nxt -lt $nr ]; do
 			if [ $(($nxt % 5)) -eq 0 ]; then
 			    echo -e "\t.dword sys_ni_syscall\t/* $nxt */"
 			else
@@ -68,16 +68,16 @@ elif [ ${out: -2} = ".S" ]; then
 		    else
 			echo -e "\t.dword ${entry_64}"
 		    fi
-		    nxt="$nr"
+		    nxt=$nr
 		    let nxt=nxt+1
 		fi
-	    elif [ $3 -eq 32 ]; then
+	    elif [ "$3" = "32" ]; then
 		if [ $nr -eq 0 ]; then
 		    echo -e "90:     .word ${entry_32}\t/* $nr */"
 		elif [ $nr -eq 1 ]; then
 		    echo -e "91:     .word ${entry_32}"
 		else
-		    while [ "$nxt" -lt "$nr" ]; do
+		    while [ $nxt -lt $nr ]; do
 			if [ $(($nxt % 5)) -eq 0 ]; then
 			    echo -e "\t.word sys_ni_syscall\t/* $nxt */"
 			else
@@ -90,16 +90,16 @@ elif [ ${out: -2} = ".S" ]; then
 		    else
 			echo -e "\t.word ${entry_32}"
 		    fi
-		    nxt="$nr"
+		    nxt=$nr
 		    let nxt=nxt+1
 		fi
-	    elif [ $3 -eq x32 ]; then
+	    elif [ "$3" = "x32" ]; then
 		if [ $nr -eq 0 ]; then
 		    echo -e "90:     .dword ${entry_x32}\t/* $nr */"
 		elif [ $nr -eq 1 ]; then
 		    echo -e "91:     .dword ${entry_x32}"
 		else
-		    while [ "$nxt" -lt "$nr" ]; do
+		    while [ $nxt -lt $nr ]; do
 			if [ $(($nxt % 5)) -eq 0 ]; then
 			    echo -e "\t.dword sys_ni_syscall\t/* $nxt */"
 			else
@@ -112,7 +112,7 @@ elif [ ${out: -2} = ".S" ]; then
 		    else
 			echo -e "\t.dword ${entry_x32}"
 		    fi
-		    nxt="$nr"
+		    nxt=$nr
 		    let nxt=nxt+1
 		fi
 	    fi
