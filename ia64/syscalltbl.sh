c49393c58063a8f4ebc68f56e6ab6856e4b4d5ea
Firoz Khan <firozk@codeaurora.org>
2018-05-30 09:32:39 +0530

    arch: ia64: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/syscall_table.S
    - Remove comment
    - Cleanup the script

--- ia64/syscalltbl.sh
+++ ia64/syscalltbl.sh
@@ -7,77 +7,92 @@ my_abis=`echo "($3)" | tr ',' '|'`
 prefix="$4"
 offset="$5"
 
-if [ "${out: -2}" = ".h" ]; then
+tbl_footer() {
+    echo ""
+    echo -e "\t.org sys_call_table + 8*NR_syscalls\t/* guard against failures to increase NR_syscalls */"
+}
+
+tbl_out() {
+    nxt="$1"
+    nr="$2"
+    entry="$3"
+
+    while [ $nxt -lt $nr ]; do
+	echo -e "\tdata8 sys_ni_syscall"
+	let nxt=nxt+1
+    done
+
+    echo -e "\tdata8 $entry"
+}
+
+tbl_header() {
+    echo "/* SPDX-License-Identifier: GPL-2.0 */"
+    echo -e "\t.rodata"
+    echo -e "\t.align 8"
+    echo -e "\t.globl sys_call_table"
+    echo "sys_call_table:"
+}
+
+hdr_footer() {
+    echo ""
+    echo "#endif /* ${fileguard} */"
+}
+
+hdr_out() {
+    nr="$1"
+    name="$2"
+
+    if [ -z "$offset" ]; then
+	echo -e "#define __NR_${prefix}${name}\t$nr"
+    else
+	echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$const)))"
+    fi
+}
+
+hdr_header() {
+    echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
+    echo "/*"
+    echo " * IA-64 Linux syscall numbers and inline-functions."
+    echo " *"
+    echo " * Copyright (C) 1998-2005 Hewlett-Packard Co"
+    echo " * David Mosberger-Tang <davidm@hpl.hp.com>"
+    echo " */"
+    echo "#ifndef ${fileguard}"
+    echo "#define ${fileguard}"
+    echo ""
+    echo "#include <asm/break.h>"
+    echo ""
+    echo -e "#define __BREAK_SYSCALL\t__IA64_BREAK_SYSCALL"
+    echo ""
+}
+
+hdr_fileguard() {
     fileguard=_UAPI_ASM_IA64_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
+}
+
+if [ "${out: -2}" = ".h" ]; then
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
-	echo "/*"
-	echo " * IA-64 Linux syscall numbers and inline-functions."
-	echo " *"
-	echo " * Copyright (C) 1998-2005 Hewlett-Packard Co"
-	echo " * David Mosberger-Tang <davidm@hpl.hp.com>"
-	echo " */"
-	echo "#ifndef ${fileguard}"
-	echo "#define ${fileguard}"
-	echo ""
-	echo "#include <asm/break.h>"
-	echo ""
-	echo -e "#define __BREAK_SYSCALL\t__IA64_BREAK_SYSCALL"
-	echo ""
+	hdr_fileguard
+	hdr_header
 
 	while read nr abi name entry comment ; do
-	    if [ -z "$offset" ]; then
-		if [ "$name" != "-" ]; then
-		    if [ -z "$comment" ]; then
-			echo -e "#define __NR_${prefix}${name}\t$nr"
-		    else
-			echo -e "/* $nr was __NR_${prefix}${name} */"
-		    fi
-		fi
-	    else
-		if [ "$name" != "-" ]; then
-		    if [ -z "$comment" ]; then
-			echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
-		    else
-			echo -e "/* $nr was __NR_${prefix}${name} */"
-		    fi
-		fi
-            fi
+	    hdr_out $nr $name
 	done
 
-	echo ""
-	echo "#endif /* ${fileguard} */"
+	hdr_footer
     ) > "$out"
 elif [ "${out: -2}" = ".S" ]; then
     nxt=1024
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	echo "/* SPDX-License-Identifier: GPL-2.0 */"
-	echo -e "\t.rodata"
-	echo -e "\t.align 8"
-	echo -e "\t.globl sys_call_table"
-	echo "sys_call_table:"
+	tbl_header
 
 	while read nr abi name entry comment ; do
-	    while [ $nxt -lt $nr ]; do
-		if [ $(($nxt % 5)) -eq 0 ]; then
-		    echo -e "\tdata8 sys_ni_syscall\t/* $nxt */"
-		else
-		    echo -e "\tdata8 sys_ni_syscall"
-		fi
-		let nxt=nxt+1
-	    done
-	    if [ $(($nxt % 5)) -eq 0 ]; then
-		echo -e "\tdata8 ${entry}\t/* $nxt */"
-	    else
-		echo -e "\tdata8 ${entry}"
-	    fi
-	    nxt=$nr
-	    let nxt=nxt+1
+	    tbl_out $nxt $nr $entry
+            let nxt=nxt+1
 	done
 
-	echo ""
-	echo -e "\t.org sys_call_table + 8*NR_syscalls\t/* guard against failures to increase NR_syscalls */"
+	tbl_footer
     ) > "$out"
 fi
