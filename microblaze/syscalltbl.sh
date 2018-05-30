daf7660fd9ffea90d8913f02c3d1db6f06dc2f27
Firoz Khan <firozk@codeaurora.org>
2018-05-30 10:10:33 +0530

    arch: microblaze: Add system call table generator to generate include/uapi/asm/unistd.h and kernel/syscall_table.S
    - Remove comment
    - Cleanup the script

--- microblaze/syscalltbl.sh
+++ microblaze/syscalltbl.sh
@@ -7,70 +7,81 @@ my_abis=`echo "($3)" | tr ',' '|'`
 prefix="$4"
 offset="$5"
 
-if [ "${out: -2}" = ".h" ]; then
+tbl_out() {
+    nxt="$1"
+    nr="$2"
+    entry="$3"
+
+    while [ $nxt -lt $nr ]; do
+	echo -e "\t.long sys_ni_syscall"
+	let nxt=nxt+1
+    done
+
+    echo -e "\t.long $entry"
+}
+
+tbl_header() {
+    echo "/* SPDX-License-Identifier: GPL-2.0 */"
+    echo "ENTRY(sys_call_table)"
+}
+
+hdr_footer() {
+    echo ""
+    echo "#endif /* ${fileguard} */"
+}
+
+hdr_out() {
+    nr="$1"
+    name="$2" 
+
+    if [ -z "$offset" ]; then
+	echo -e "#define __NR_${prefix}${name}\t$nr"
+    else
+	echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$const)))"
+    fi
+}
+
+hdr_header() {
+    echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
+    echo "/*"
+    echo " * Copyright (C) 2007-2008 Michal Simek <monstr@monstr.eu>"
+    echo " * Copyright (C) 2006 Atmark Techno, Inc."
+    echo " *"
+    echo " * This file is subject to the terms and conditions of the GNU General Public"
+    echo " * License. See the file "\"COPYING"\" in the main directory of this archive"
+    echo " * for more details."
+    echo " */"
+    echo ""
+    echo "#ifndef ${fileguard}"
+    echo "#define ${fileguard}"
+    echo ""
+}
+
+hdr_fileguard() {
     fileguard=_UAPI_ASM_MICROBLAZE_`basename "$out" | sed \
     -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
     -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
+}
+
+if [ "${out: -2}" = ".h" ]; then
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
-	echo "/*"
-	echo " * Copyright (C) 2007-2008 Michal Simek <monstr@monstr.eu>"
-	echo " * Copyright (C) 2006 Atmark Techno, Inc."
-	echo " *"
-	echo " * This file is subject to the terms and conditions of the GNU General Public"
-	echo " * License. See the file "\"COPYING"\" in the main directory of this archive"
-	echo " * for more details."
-	echo " */"
-	echo ""
-	echo "#ifndef ${fileguard}"
-	echo "#define ${fileguard}"
-	echo ""
+	hdr_fileguard
+	hdr_header
 
 	while read nr abi name entry comment ; do
-	    if [ -z "$offset" ]; then
-		if [ -z "$comment" ]; then
-		    echo -e "#define __NR_${prefix}${name}\t$nr"
-		elif [ "$comment" = "-" ]; then
-		    echo -e "/* #define __NR_${prefix}${name}\t$nr */"
-		else
-		    echo -e "#define __NR_${prefix}${name}\t$nr\t$comment"
-		fi
-	    else
-		if [ -z "$comment" ]; then
-		    echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
-		elif [ "$comment" = "-" ]; then
-		    echo -e "/* #define __NR_${prefix}${name}\t($offset + $nr) */"
-		else
-		    echo -e "#define __NR_${prefix}${name}\t($offset + $nr)\t$comment"
-		fi
-            fi
+	    hdr_out $nr $name
 	done
 
-	echo ""
-	echo "#endif /* ${fileguard} */"
+	hdr_footer
     ) > "$out"
 elif [ "${out: -2}" = ".S" ]; then
     nxt=0
     grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in" | sort -n | (
-	echo "/* SPDX-License-Identifier: GPL-2.0 */"
-	echo "ENTRY(sys_call_table)"
+	tbl_header
 
 	while read nr abi name entry comment ; do
-	    while [ $nxt -lt $nr ]; do
-		if [ $(($nxt % 5)) -eq 0 ]; then
-		    echo -e "\t.long sys_ni_syscall\t/* $nxt */"
-		else
-		    echo -e "\t.long sys_ni_syscall"
-		fi
-		let nxt=nxt+1
-	    done
-	    if [ $(($nr % 5)) -eq 0 ]; then
-		echo -e "\t.long ${entry}\t/* $nr */"
-	    else
-		echo -e "\t.long ${entry}"
-	    fi
-	    nxt=$nr
-	    let nxt=nxt+1
+	    tbl_out $nxt $nr $entry
+            let nxt=nxt+1
 	done
     ) > "$out"
 fi
