e358fb2608535e0ed780180752ab0f4d827906a6
Firoz Khan <firozk@codeaurora.org>
2018-06-05 14:12:28 +0530

    arch: parisc: System call table generator
    - Generate include/generated/uapi/asm/unistd.h and
      include/generated/asm/syscalls_64/32/x32.h
    - Add kernel/syscall_64/32/x32.S file which will in turn
      include include/generated/asm/syscalls_64/32/x32.h file

--- parisc/README.md
+++ parisc/README.md
@@ -1,4 +1,16 @@
-# system_call_table_generator
+# parisc: system_call_table_generator
 
-This script will generate arch/parisc/include/uapi/asm/unistd.h
-and arch/parisc/kernel/syscall_table.S by reading syscall.tbl file
+- This architecture does have single ABI.
+  syscall.tbl contains the information like 
+    - system call number
+    - name 
+    - entry_64/entry_32
+    - compat
+    - comments
+
+- The scripts syscallhdr.sh will generate uapi header- 
+  arch/parisc/include/uapi/asm/unistd.h. 
+
+- The script syscalltbl.sh will generate syscalls.h 
+  which will be included by syscall_64/32/x32.S file
+
--- parisc/syscalltbl.sh
+++ parisc/syscalltbl.sh
@@ -7,113 +7,28 @@ abi="$3"
 prefix="$4"
 offset="$5"
 
-tbl_footer() {
-    echo ""
-    echo ".ifne (. - 90b) - (__NR_Linux_syscalls * (91b - 90b))"
-    echo ".error "\"size of syscall table does not fit value of __NR_Linux_syscalls"\""
-    echo ".endif"
-}
-
-tbl_out() {
+emit() {
     nxt="$1"
     nr="$2"
     entry="$3"
-    dtype="$4"
-
-    if [ $nr -eq 0 ]; then
-	echo -e "90:     $dtype $entry"
-    elif [ $nr -eq 1 ]; then
-	echo -e "91:     $dtype $entry"
-    else
-	while [ $nxt -lt $nr ]; do
-	    echo -e "\t$dtype sys_ni_syscall"
-	    let nxt=nxt+1
-	done
-	echo -e "\t$dtype $entry"
-    fi
+    
+    while [ $nxt -lt $nr ]; do
+	echo "__SYSCALL($nxt, sys_ni_syscall, )"
+        let "nxt=nxt+1"
+    done
+    
+    echo "__SYSCALL($nr, $entry, )"
 }
 
-tbl_header() {
-    echo "/* SPDX-License-Identifier: GPL-2.0 */"    
-}
-
-hdr_footer() {
-    echo ""
-    echo -e "#define __NR_Linux_syscalls\t(__NR_statx + 1)"
-    echo ""
-    echo -e "#define LINUX_GATEWAY_ADDR\t0x100"
-    echo ""
-    echo "#endif /* ${fileguard} */"
-}
-
-hdr_out() {
-    nr="$1"
-    name="$2"
-
-    if [ -z "$offset" ]; then
-	echo -e "#define __NR_${prefix}${name}\t$nr"
-    else
-	echo -e "#define __NR_${prefix}${name}\t($offset + $nr)"
-    fi
-}
-
-hdr_header() {
-    echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
-    echo "#ifndef ${fileguard}"
-    echo "#define ${fileguard}"
-    echo ""
-    echo "/*"
-    echo " * Linux system call numbers."
-    echo " *"
-    echo " * Cary Coutant says that we should just use another syscall gateway"
-    echo " * page to avoid clashing with the HPUX space, and I think he's right:"
-    echo " * it will would keep a branch out of our syscall entry path, at the"
-    echo " * very least.  If we decide to change it later, we can "\"just"\" tweak"
-    echo " * the LINUX_GATEWAY_ADDR define at the bottom and make __NR_Linux be"
-    echo " * 1024 or something.  Oh, and recompile libc. =)"
-    echo " */"
-    echo ""
-    echo -e "#define __NR_Linux\t0"
-}
-
-hdr_fileguard() {
-    fileguard=_UAPI_ASM_PARISC_`basename "$out" | sed \
-    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
-    -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`_    
-}
-
-if [ "${out: -2}" = ".h" ]; then
-    grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in" | sort -n | (
-	hdr_fileguard
-	hdr_header
-
-	while read nr name entry compat comment ; do
-	    hdr_out $nr $name
-	done
-
-	hdr_footer
-    ) > "$out"
-elif [ "${out: -2}" = ".S" ]; then
-    nxt=2
-    grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in" | sort -n | (
-	tbl_header
-	
-	while read nr name entry compat comment ; do
-	    if [ "$3" = "64" ]; then
-		tbl_out $nxt $nr $entry ".dword"
-	       	nxt=$nr
-		let nxt=nxt+1
-	    elif [ "$3" = "32" ]; then
-		tbl_out $nxt $nr $entry ".word"
-                nxt=$nr
-                let nxt=nxt+1
-	    elif [ "$3" = "x32" ]; then
-		tbl_out $nxt $nr $compat ".dword"
-                nxt=$nr
-                let nxt=nxt+1
-	    fi
-	done
-	
-	tbl_footer
-    ) > "$out"
-fi
+grep '^[0-9]' "$in" | sort -n | (
+    nxt=0
+    while read nr name entry compat comment ; do
+	if [ "$abi" = "64" -o "$abi" = "32" ]; then
+            emit $nxt $nr $entry
+	elif [ "$abi" = "x32" ]; then
+            emit $nxt $nr $compat
+	fi
+	nxt=$nr
+        let "nxt=nxt+1"
+    done
+) > "$out"
