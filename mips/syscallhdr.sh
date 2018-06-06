0c87d23b30a7b746b0f77863296ef25b43b6501e
Firoz Khan <firozk@codeaurora.org>
2018-06-06 10:29:05 +0530

    arch: mips: System call table generator
    - Generate include/generated/uapi/asm/unistd.h and
      include/generated/asm/syscalls_64/32/o32/n32.h
    - Add kernel/syscall_64/32/x32.S file which will in turn
      include include/generated/asm/syscalls_64/32/o32/n32.h file

--- mips/syscallhdr.sh
+++ mips/syscallhdr.sh
@@ -9,11 +9,29 @@ abi="$5"
 prefix="$6"
 offset="$7"
 
-hdr_footer_32() {
+fileguard=_UAPI_ASM_`basename "$out" | sed \
+    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
+    -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
+grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in_32" | sort -n | (
+    echo "#ifndef ${fileguard}"
+    echo "#define ${fileguard}"
     echo ""
-    echo "/*"
-    echo " * Offset of the last Linux o32 flavoured syscall"
-    echo " */"
+    echo "#include <asm/sgidefs.h>"
+    echo ""
+    echo "#if _MIPS_SIM == _MIPS_SIM_ABI32"
+    echo ""
+    echo -e "#define __NR_Linux\t4000"
+    
+    base=4000
+    while read nr name entry compat comment ; do
+	if [ -z "$offset" ]; then
+	    echo -e "#define __NR_${prefix}${name}\t$nr"
+	else
+	    echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$base)))"
+	fi
+    done
+
+    echo ""
     echo -e "#define __NR_Linux_syscalls\t366"
     echo ""
     echo "#endif /* _MIPS_SIM == _MIPS_SIM_ABI32 */"
@@ -21,78 +39,23 @@ hdr_footer_32() {
     echo -e "#define __NR_O32_Linux\t4000"
     echo -e "#define __NR_O32_Linux_syscalls\t366"
     echo ""
-}
-
-hdr_out() {
-    nr="$1"
-    name="$2"
-    base="$3"
-
-    if [ -z "$offset" ]; then
-	echo -e "#define __NR_${prefix}${name}\t$nr"
-    else
-	echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$base)))"
-    fi
-}
-
-hdr_header_32() {
-    echo "#if _MIPS_SIM == _MIPS_SIM_ABI32"
-    echo ""
-    echo "/*"
-    echo " * Linux o32 style syscalls are in the range from 4000 to 4999."
-    echo " */"
-    echo -e "#define __NR_Linux\t4000"
-}
-
-hdr_header() {
-    echo "/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */"
-    echo "/*"
-    echo " * This file is subject to the terms and conditions of the GNU General Public"
-    echo " * License.  See the file "\"COPYING"\" in the main directory of this archive"
-    echo " * for more details."
-    echo " *"
-    echo " * Copyright (C) 1995, 96, 97, 98, 99, 2000 by Ralf Baechle"
-    echo " * Copyright (C) 1999, 2000 Silicon Graphics, Inc."
-    echo " *"
-    echo " * Changed system calls macros _syscall5 - _syscall7 to push args 5 to 7 onto"
-    echo " * the stack. Robin Farine for ACN S.A, Copyright (C) 1996 by ACN S.A"
-    echo " */"
-    echo "#ifndef ${fileguard}"
-    echo "#define ${fileguard}"
-    echo ""
-    echo "#include <asm/sgidefs.h>"
-    echo ""
-}
-
-hdr_fileguard() {
-    fileguard=_UAPI_ASM_`basename "$out" | sed \
-    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
-    -e 's/[^A-Z0-9_]/_/g' -e 's/__/_/g'`
-}
-
-grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in_32" | sort -n | (
-    hdr_fileguard
-    hdr_header
-    hdr_header_32
-    
-    base=4000
-    while read nr name entry compat comment ; do
-	 hdr_out $nr $name $base
-    done
-
-    hdr_footer_32
 ) > "$out"
 
-hdr_footer() {
+grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in_64" | sort -n | (
+    echo "#if _MIPS_SIM == _MIPS_SIM_ABI64"
     echo ""
-    echo "#endif /* ${fileguard} */"
-}
+    echo -e "#define __NR_Linux\t5000"
 
-hdr_footer_64() {
+    base=5000
+    while read nr name entry comment ; do
+	if [ -z "$offset" ]; then
+	    echo -e "#define __NR_${prefix}${name}\t$nr"
+	else
+	    echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$base)))"
+	fi
+    done
+
     echo ""
-    echo "/*"
-    echo " * Offset of the last Linux 64-bit flavoured syscall"
-    echo " */"
     echo -e "#define __NR_Linux_syscalls\t326"
     echo ""
     echo "#endif /* _MIPS_SIM == _MIPS_SIM_ABI64 */"
@@ -100,59 +63,28 @@ hdr_footer_64() {
     echo -e "#define __NR_64_Linux\t5000"
     echo -e "#define __NR_64_Linux_syscalls\t326"
     echo ""
-}
-
-hdr_header_64() {
-    echo "#if _MIPS_SIM == _MIPS_SIM_ABI64"
-    echo ""
-    echo "/*"
-    echo " * Linux 64-bit syscalls are in the range from 5000 to 5999."
-    echo " */"
-    echo -e "#define __NR_Linux\t5000"
-}
-
-grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in_64" | sort -n | (
-    hdr_header_64
-
-    base=5000
-    while read nr name entry comment ; do
-	hdr_out $nr $name $base
-    done
-
-    hdr_footer_64
 ) >> "$out"
 
-hdr_footer_n32() {
+grep -E "^[0-9A-Fa-fXx]+[[:space:]]+${my_abis}" "$in_n32" | sort -n | (
+    echo "#if _MIPS_SIM == _MIPS_SIM_NABI32"
     echo ""
-    echo "/*"
-    echo " * Offset of the last N32 flavoured syscall"
-    echo " */"
+    echo -e "#define __NR_Linux\t6000"
+
+    base=6000
+    while read nr name entry comment ; do
+	if [ -z "$offset" ]; then
+	    echo -e "#define __NR_${prefix}${name}\t$nr"
+	else
+	    echo -e "#define __NR_${prefix}${name}\t($offset + $(($nr-$base)))"
+	fi
+    done
+
     echo -e "#define __NR_Linux_syscalls\t330"
     echo ""
     echo "#endif /* _MIPS_SIM == _MIPS_SIM_NABI32 */"
     echo ""
     echo -e "#define __NR_N32_Linux\t6000"
     echo -e "#define __NR_N32_Linux_syscalls\t330"
-}
-
-hdr_header_n32() {
-    echo "#if _MIPS_SIM == _MIPS_SIM_NABI32"
     echo ""
-    echo "/*"
-    echo " * Linux N32 syscalls are in the range from 6000 to 6999."
-    echo " */"
-    echo -e "#define __NR_Linux\t6000"
-}
-
-grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in_n32" | sort -n | (
-    hdr_fileguard
-    hdr_header_n32
-
-    base=6000
-    while read nr name entry comment ; do
-	hdr_out $nr $name $base
-    done
-
-    hdr_footer_n32
-    hdr_footer
+    echo "#endif /* ${fileguard} */"
 ) >> "$out"
--- mips/syscalltbl.sh
+++ mips/syscalltbl.sh
@@ -4,73 +4,34 @@
 in="$1"
 out="$2"
 abi="$3"
-prefix="$4"
-offset="$5"
 
-tbl_footer() {
-    abi="$1"
-
-    if [ "$abi" = "64-o32" ]; then
-	echo -e "\t.size\tsys32_call_table,.-sys32_call_table"
-    elif [ "$abi" = "64-64" ]; then
-	echo -e "\t.size\tsys_call_table,.-sys_call_table"
-    elif [ "$abi" = "64-n32" ]; then
-	echo -e "\t.size\tsysn32_call_table,.-sysn32_call_table"
-    fi
-}
-
-tbl_out() {
+emit() {
     nxt="$1"
     nr="$2"
     entry="$3"
     
     while [ $nxt -lt $nr ]; do
-        echo -e "\tPTR\tsys_ni_syscall"
-        let nxt=nxt+1
+	echo "__SYSCALL($nxt, sys_ni_syscall, )"
+        let "nxt=nxt+1"
     done
     
-    echo -e "\tPTR\t$entry"
-}
-
-tbl_header() {
-    abi="$1"
-
-    echo "/* SPDX-License-Identifier: GPL-2.0 */"
-    echo ""
-    if [ "$abi" = "32-o32" ]; then
-	nxt=4000
-	echo -e "\t.align\t2"
-	echo -e "\t.type\tsys_call_table, @object"
-	echo "EXPORT(sys_call_table)"
-    elif [ "$abi" = "64-o32" ]; then
-	nxt=4000
-        echo -e "\t.align\t3"
-        echo -e "\t.type\tsys32_call_table,@object"
-        echo "EXPORT(sys32_call_table)"
-    elif [ "$abi" = "64-64" ]; then
-	nxt=5000
-	echo -e "\t.align\t3"
-        echo -e "\t.type\tsys_call_table, @object"
-        echo "EXPORT(sys_call_table)"
-    elif [ "$abi" = "64-n32" ]; then
-	nxt=6000
-        echo -e "\t.type\tsysn32_call_table, @object"
-        echo "EXPORT(sysn32_call_table)"
-    fi
+    echo "__SYSCALL($nr, $entry, )"
 }
 
 grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in" | sort -n | (
-    tbl_header $abi
-
+    nxt=4000
     while read nr name entry compat comment ; do
-	if [ "$abi" = "64-o32" ]; then
-            tbl_out $nxt $nr $compat
-	    let nxt=nxt+1
-	else
-            tbl_out $nxt $nr $entry
-            let nxt=nxt+1
+	if [ "$abi" = "32-o32" -o "$abi" = "64-o32" ]; then
+	    let t_nxt=$nxt+0
+            emit $t_nxt $nr $entry
+	elif [ "$abi" = "64-64" ]; then
+	    let t_nxt=$nxt+1000
+            emit $t_nxt $nr $entry
+	elif [ "$abi" = "64-n32" ]; then
+	    let t_nxt=$nxt+2000
+            emit $t_nxt $nr $entry
 	fi
+	nxt=$nr
+        let "nxt=nxt+1"
     done
-    
-    tbl_footer $abi
 ) > "$out"
