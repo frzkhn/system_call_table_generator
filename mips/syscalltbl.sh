1e252806b30b843e29265f3fb12ca1358815496b
Firoz Khan <firozk@codeaurora.org>
2018-06-19 15:55:27 +0530

    arch: mips: System call table generator
    - typo fixed

--- mips/syscalltbl.sh
+++ mips/syscalltbl.sh
@@ -21,9 +21,12 @@ emit() {
 grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$in" | sort -n | (
     nxt=4000
     while read nr name entry compat comment ; do
-	if [ "$abi" = "32-o32" -o "$abi" = "64-o32" ]; then
+	if [ "$abi" = "32-o32" ]; then 
 	    let t_nxt=$nxt+0
             emit $t_nxt $nr $entry
+	elif [ "$abi" = "64-o32" ]; then
+	    let t_nxt=$nxt+0
+            emit $t_nxt $nr $compat
 	elif [ "$abi" = "64-64" ]; then
 	    let t_nxt=$nxt+1000
             emit $t_nxt $nr $entry
