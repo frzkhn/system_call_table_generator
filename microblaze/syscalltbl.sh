2af58fab9c17e22ea16fa68e0d712bd8e3e45494
Firoz Khan <firozk@codeaurora.org>
2018-07-01 18:14:52 +0530

    arch: microblaze: System call table generator
    - "comment" removed
    - testing not done
    - added on 4.18-rc2

--- microblaze/syscalltbl.sh
+++ microblaze/syscalltbl.sh
@@ -3,7 +3,7 @@
 
 in="$1"
 out="$2"
-abi="$3"
+my_abi="$3"
 
 emit() {
     nxt="$1"
@@ -12,7 +12,7 @@ emit() {
 
     while [ $nxt -lt $nr ]; do
 	echo "__SYSCALL($nxt, sys_ni_syscall, )"
-	let "nxt=nxt+1"
+	let nxt=nxt+1
     done
 
     echo "__SYSCALL($nr, $entry, )"
@@ -20,9 +20,9 @@ emit() {
 
 grep '^[0-9]' "$in" | sort -n | (
     nxt=0
-    while read nr abi name entry comment ; do
+    while read nr abi name entry ; do
 	emit $nxt $nr $entry
 	nxt=$nr
-        let "nxt=nxt+1"
+        let nxt=nxt+1
     done
 ) > "$out"
